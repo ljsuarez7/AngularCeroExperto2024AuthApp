f059eb2453287bc8e61c5a8866202922d4601781
Javier Suárez <ljsuarez7@gmail.com>
2024-08-01 23:16:24 +0200

    implementados guards para no dejar entrar al dashboard sin estar autenticado y no dejar ir al login si lo estas, funcionalidad del login y logout hechas

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -8,14 +12,26 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginPageComponent {
 
   private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
   public myForm: FormGroup = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    email: ['fernando@google.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
 
   login(){
-    console.log(this.myForm.value);
+
+    const {email, password} = this.myForm.value;
+
+    this.authService.login(email, password)
+      .subscribe({
+        next: () => this.router.navigateByUrl('/dashboard'),
+        error: (message) => {
+          Swal.fire('Error', message, 'error');
+        }
+      });
+
   }
 
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { AuthStatus, User } from '../interfaces';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
+import { AuthStatus, CheckTokenResponse, LoginResponse, User } from '../interfaces';
 
 
 @Injectable({
@@ -16,10 +16,59 @@ export class AuthService {
   private _currentUser = signal<User|null>(null);
   private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
-  constructor() { }
+  public currentUser = computed(() => this._currentUser());
+  public authStatus = computed(() => this._authStatus());
+
+  constructor() {
+    this.checkAuthStatus().subscribe();
+  }
+
+  private setAuthentication(user: User, token: string): boolean{
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+    localStorage.setItem('token', token);
+    return true;
+  }
 
   login(email: string, password: string): Observable<boolean>{
-    return of(true);
+
+    const url = `${this.baseUrl}/auth/login`;
+    const body = {email, password};
+
+    return this.http.post<LoginResponse>(url, body)
+      .pipe(
+        map(({user, token}) => this.setAuthentication(user, token)),
+        catchError(err => throwError(() => err.error.message))
+      );
+
+  }
+
+  checkAuthStatus(): Observable<boolean>{
+
+    const url = `${this.baseUrl}/auth/check-token`;
+    const token = localStorage.getItem('token');
+
+    if(!token) {
+      this.logout();
+      return of(false);
+    }
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<CheckTokenResponse>(url, {headers})
+      .pipe(
+        map(({user, token}) => this.setAuthentication(user, token)),
+        catchError( () => {
+          this._authStatus.set(AuthStatus.notAuthenticated);
+          return of(false);
+        })
+      )
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    this._currentUser.set(null);
+    this._authStatus.set(AuthStatus.notAuthenticated);
   }
 
 }
